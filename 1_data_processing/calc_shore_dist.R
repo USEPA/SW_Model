facaa8a1e41a925d7c2efd34b07ee12c3dd77f96
natalievtr <vontress.natalie@epa.gov>
2023-06-13 15:56:46 -0400

    Edited code to make output consistent with other files.

--- 1_data_processing/calc_shore_dist.R
+++ 1_data_processing/calc_shore_dist.R
@@ -35,37 +35,44 @@ lakes_resolvable <- lakes[which(lakes$max_window >= min_size), ]
 nla_07 <- read_feather("data/NLA/NLA_2007_sample_dates_locations.feather") %>% 
   mutate(DATE_COL = as.Date(DATE_COL, "%m/%d/%Y")) %>% select(COMID, DATE_COL, TEMPERATURE, DEPTH, LAT_DD83, LON_DD83) %>%
   rename(date = DATE_COL,
-         LAT = LAT_DD83,
-         LONG = LON_DD83) %>%
-  mutate(source = "NLA")
+         depth = DEPTH,
+         lat = LAT_DD83,
+         long = LON_DD83,
+         insitu_temp = TEMPERATURE) %>%
+  mutate(data_set = "NLA")
 
 nla_12 <- read_feather("data/NLA/NLA_2012_sample_dates_locations.feather") %>%
   mutate(DATE_COL = as.Date(DATE_COL, "%m/%d/%Y")) %>% select(COMID, DATE_COL, TEMPERATURE, DEPTH, LAT_DD83, LON_DD83) %>%
   rename(date = DATE_COL,
-         LAT = LAT_DD83,
-         LONG = LON_DD83) %>%
-  mutate(source = "NLA")
+         depth = DEPTH,
+         lat = LAT_DD83,
+         long = LON_DD83,
+         insitu_temp = TEMPERATURE) %>%
+  mutate(data_set = "NLA")
 
 nla_17 <- read_feather("data/NLA/NLA_2017_sample_dates_locations.feather") %>%
   mutate(DATE_COL = as.Date(DATE_COL, "%m/%d/%Y")) %>% select(COMID, DATE_COL, TEMPERATURE, DEPTH, LAT_DD83, LON_DD83) %>%
   rename(date = DATE_COL,
-         LAT = LAT_DD83,
-         LONG = LON_DD83) %>%
-  mutate(source = "NLA")
+         depth = DEPTH,
+         lat = LAT_DD83,
+         long = LON_DD83,
+         insitu_temp = TEMPERATURE) %>%
+  mutate(data_set = "NLA")
 
 nwis <- read_feather("data/WQP/NWIS_sample_dates_locations.feather") %>% select(COMID, StartDate, Temp, Depth, Lat, Long) %>%
-  rename(date = StartDate, TEMPERATURE = Temp, DEPTH = Depth,
-         LAT = Lat,
-         LONG = Long) %>% 
+  rename(date = StartDate, 
+         insitu_temp = Temp, 
+         depth = Depth,
+         lat = Lat,
+         long = Long) %>% 
   mutate(date = as.Date(date, "%Y-%m-%d"),
-         source = "NWIS") %>%
-  filter(TEMPERATURE < 45)
+         data_set = "NWIS") 
 
 insitu <- rbind(nla_07, nla_12, nla_17, nwis)
 
 # specify longitude and latitude variable names in in situ file
-long_name <- "LONG" # <<<<<<
-lat_name <- "LAT" # <<<<<<
+long_name <- "long" # <<<<<<
+lat_name <- "lat" # <<<<<<
 
 # get coordinates and use to make SpatialPointsDataFrame object
 coords <- coordinates(data.frame(long = insitu[, long_name], lat = insitu[, lat_name])) 
@@ -89,6 +96,9 @@ for (r in seq_along(samp_pts_albers)) {
   samp_pts_albers$dist_shore_m[r] <- gDistance(samp_pts_albers[r, ], lakes_lines)
 }
 
+
+samp_pts_albers@data$lat <- samp_pts_albers@coords[,2]
+samp_pts_albers@data$long <- samp_pts_albers@coords[,1]
 
 # output as csv - output is input table with distance column appended
 # write.csv(samp_pts_albers@data, "data/insitu_data_dist_shore.csv") # <<<<<<
